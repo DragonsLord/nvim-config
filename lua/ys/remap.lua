ab9a892b5eb05ab4ca9a1c69f584613a95cb9dfc
Yevhenii Serdiuk <zheka2797@gmail.com>
2024-11-03 16:03:11 +0200

    neotree & fterm

--- lua/ys/remap.lua
+++ lua/ys/remap.lua
@@ -1,17 +1,23 @@
-vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "[F]ile [E]xplorer" })
+-- vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "[F]ile [E]xplorer" })
 
 -- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 -- vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- window navigation remap
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 -- do not replace the yank work with pasting over
 vim.keymap.set("x", "<leader>p", '"_dP', { desc = "[P]aste without pasting over" })
 
 -- start replace op on the current word
 vim.keymap.set(
-	"n",
-	"<leader>r",
-	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-	{ desc = "[R]eplace On Current Word" }
+  "n",
+  "<leader>r",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "[R]eplace On Current Word" }
 )
 
 -- save current buffer
