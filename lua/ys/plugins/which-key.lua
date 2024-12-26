5c3cbfaf3776e0ec01fb21bdb51901aad48b3dfd
Yevhenii Serdiuk <yevheniiserdiuk@gmail.com>
2024-12-26 11:55:57 +0200

    chore: add missing key groups descriptions

--- lua/ys/plugins/which-key.lua
+++ lua/ys/plugins/which-key.lua
@@ -1,27 +1,29 @@
 return {
-  "folke/which-key.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "echasnovski/mini.icons",
-  },
-  opts = {
-    spec = {
-      {
-        mode = { "n", "v" },
-        { "<leader>f", group = "[F]iles" },
-        { "<leader>g", group = "[G]it" },
-        { "<leader>c", group = "[C]ode" },
-      },
-    },
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Show Local Buffer Keymaps",
-    },
-  },
+	"folke/which-key.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"echasnovski/mini.icons",
+	},
+	opts = {
+		spec = {
+			{
+				mode = { "n", "v" },
+				{ "<leader>f", group = "[F]iles" },
+				{ "<leader>g", group = "[G]it" },
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]iagnostics" },
+				{ "<leader>s", group = "[S]ession" },
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Show Local Buffer Keymaps",
+		},
+	},
 }
